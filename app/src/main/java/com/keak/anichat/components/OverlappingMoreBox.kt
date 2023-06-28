8ada95498e0ec50dfbce6dda8f833bc02aff2140
akeskinbas <akin.keskinbas@hepsiburada.com>
2023-06-29 01:36:59 +0300

    GroupCardView and font added

---
+++ app/src/main/java/com/keak/anichat/components/OverlappingMoreBox.kt
@@ -0,0 +1,40 @@
+package com.keak.anichat.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun OverlappingMoreBox(morePeopleCount:Int) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .width(30.dp)
+            .height(30.dp)
+            .border(width = 1.dp, color = Color.Black, shape = CircleShape)
+            .clip(CircleShape)
+            .background(Color.White),
+
+        ) {
+        Text(
+            text = "$morePeopleCount+",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            textAlign = TextAlign.Center,
+            )
+    }
+}
