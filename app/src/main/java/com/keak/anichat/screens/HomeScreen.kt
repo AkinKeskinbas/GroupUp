06d429b9aaf9785a439cc5763def520b861ccd5d
akeskinbas <akin.keskinbas@hepsiburada.com>
2023-06-29 18:10:02 +0300

    Added HomeScreen response logic and tested with dummy data. Boost logic added

--- app/src/main/java/com/keak/anichat/screens/HomeScreen.kt
+++ app/src/main/java/com/keak/anichat/screens/HomeScreen.kt
@@ -1,9 +1,100 @@
 package com.keak.anichat.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
+import com.google.gson.Gson
+import com.keak.anichat.components.GroupCard
+import com.keak.anichat.components.VerticalGroupCard
+import com.keak.anichat.data.HomeResponse
+import com.keak.anichat.data.Room
+import com.keak.anichat.util.Misc
 import com.keak.geniusai.navigation.Router
+import timber.log.Timber
+import kotlin.random.Random
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(router: Router) {
+    val roomsResponse = Gson().fromJson(Misc.dummy, HomeResponse::class.java)
+    val baseRooms = remember {
+        mutableStateListOf<Room>()
+    }
 
+
+    roomsResponse.rooms?.forEach {room->
+        room?.let { safeRoom->
+            if (safeRoom.boosted==true){
+                baseRooms.add(0,safeRoom)
+            }else{
+                baseRooms.add(safeRoom)
+            }
+        }
+    }
+    val photoList = roomsResponse.rooms?.map {
+        it?.subs?.map {subs->
+            subs?.subPhoto.orEmpty()
+        }.orEmpty()
+    }.orEmpty().flatten()
+    val screenWidth = LocalConfiguration.current.screenWidthDp
+    Column (modifier = Modifier.padding(8.dp)){
+        Text(text = "Rooms", style = MaterialTheme.typography.titleLarge, color = Color.Black)
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyVerticalStaggeredGrid(
+            columns = StaggeredGridCells.Adaptive((screenWidth / 2.7).dp),
+            modifier = Modifier
+        ) {
+            Timber.tag("list").d("Gridview init")
+            items(baseRooms) {room->
+                val color = Color(
+                    red = Random.nextInt(255),
+                    green = Random.nextInt(255),
+                    blue = Random.nextInt(255),
+                    alpha = 255
+                )
+                when (room.isSpecialChatRoom) {
+                    true -> {
+                        VerticalGroupCard(
+                            bgColor = color,
+                            title = if (room.boosted == true) "Boosted" else room.title.orEmpty(),
+                            userName = room.ownerName.orEmpty(),
+                            userPhoto = room.ownerImage.orEmpty(),
+                            participantsPhotoList = photoList,
+                            modifier = Modifier.padding(8.dp)
+                        ) {
+
+                        }
+                    }
+
+                    else -> {
+                        GroupCard(
+                            bgColor = color,
+                            title = if (room.boosted == true) "Boosted" else room.title.orEmpty(),
+                            userName = room.ownerName.orEmpty(),
+                            userPhoto = room.ownerImage.orEmpty(),
+                            participantsPhotoList = photoList,
+                            modifier = Modifier.padding(8.dp)
+                        ) {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+ 
 }
---
+++ app/src/main/java/com/keak/anichat/util/Misc.kt
@@ -0,0 +1,293 @@
+package com.keak.anichat.util
+
+object Misc {
+  val dummy =  """{
+        "rooms": [
+        {
+            "title": "Pets",
+            "isOwner": true,
+            "roomId": "123",
+            "ownerImage": "https://i.pinimg.com/564x/1e/39/86/1e39866c41e9e77d962a07b4577423c7.jpg",
+            "ownerName": "Me",
+            "boosted": true,
+            "isSpecialChatRoom": false,
+            "isLimitedRoom": false,
+            "userLimit": 0,
+            "isRoomFull": false,
+            "subs": [
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/24/ec/2a/24ec2af1b40d1979c2c368387d438c6b.jpg"
+            },
+            {
+                "subName": "Heda",
+                "subPhoto": "https://i.pinimg.com/564x/22/ba/9e/22ba9e9fc4c47e323c31a58fa746797a.jpg"
+            },
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/24/ec/2a/24ec2af1b40d1979c2c368387d438c6b.jpg"
+            },
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/22/ba/9e/22ba9e9fc4c47e323c31a58fa746797a.jpg"
+            }
+            ]
+        },
+        {
+            "title": "Pets",
+            "isOwner": true,
+            "roomId": "123",
+            "ownerImage": "https://i.pinimg.com/564x/bd/9e/9f/bd9e9f0fdd1d72a88a11c70460b53f43.jpg",
+            "ownerName": "WanHeda",
+            "boosted": false,
+            "isSpecialChatRoom": true,
+            "isLimitedRoom": false,
+            "userLimit": 0,
+            "isRoomFull": false,
+           "subs": [
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/24/ec/2a/24ec2af1b40d1979c2c368387d438c6b.jpg"
+            },
+            {
+                "subName": "Heda",
+                "subPhoto": "https://i.pinimg.com/564x/22/ba/9e/22ba9e9fc4c47e323c31a58fa746797a.jpg"
+            },
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/24/ec/2a/24ec2af1b40d1979c2c368387d438c6b.jpg"
+            },
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/22/ba/9e/22ba9e9fc4c47e323c31a58fa746797a.jpg"
+            }
+            ]
+        },
+        {
+            "title": "Pets",
+            "isOwner": true,
+            "roomId": "123",
+            "ownerImage": "https://i.pinimg.com/564x/a7/de/24/a7de24dc367e240a814a47fc378e8141.jpg",
+            "ownerName": "Me",
+            "boosted": false,
+            "isSpecialChatRoom": false,
+            "isLimitedRoom": false,
+            "userLimit": 0,
+            "isRoomFull": false,
+          "subs": [
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/24/ec/2a/24ec2af1b40d1979c2c368387d438c6b.jpg"
+            },
+            {
+                "subName": "Heda",
+                "subPhoto": "https://i.pinimg.com/564x/22/ba/9e/22ba9e9fc4c47e323c31a58fa746797a.jpg"
+            },
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/24/ec/2a/24ec2af1b40d1979c2c368387d438c6b.jpg"
+            },
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/22/ba/9e/22ba9e9fc4c47e323c31a58fa746797a.jpg"
+            }
+            ]
+        },
+        {
+            "title": "Pets",
+            "isOwner": true,
+            "roomId": "123",
+            "ownerImage": "https://i.pinimg.com/564x/a7/de/24/a7de24dc367e240a814a47fc378e8141.jpg",
+            "ownerName": "Me",
+            "boosted": true,
+            "isSpecialChatRoom": false,
+            "isLimitedRoom": false,
+            "userLimit": 0,
+            "isRoomFull": false,
+          "subs": [
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/24/ec/2a/24ec2af1b40d1979c2c368387d438c6b.jpg"
+            },
+            {
+                "subName": "Heda",
+                "subPhoto": "https://i.pinimg.com/564x/22/ba/9e/22ba9e9fc4c47e323c31a58fa746797a.jpg"
+            },
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/24/ec/2a/24ec2af1b40d1979c2c368387d438c6b.jpg"
+            },
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/22/ba/9e/22ba9e9fc4c47e323c31a58fa746797a.jpg"
+            }
+            ]
+        },
+        {
+            "title": "Chill",
+            "isOwner": false,
+            "roomId": "123",
+            "ownerImage": "https://i.pinimg.com/564x/bd/9e/9f/bd9e9f0fdd1d72a88a11c70460b53f43.jpg",
+            "ownerName": "Me",
+            "boosted": false,
+            "isSpecialChatRoom": true,
+            "isLimitedRoom": true,
+            "userLimit": 15,
+            "isRoomFull": true,
+            "subs": [
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/bd/9e/9f/bd9e9f0fdd1d72a88a11c70460b53f43.jpg"
+            },
+            {
+                "subName": "Heda",
+                "subPhoto": "https://i.pinimg.com/564x/39/d1/d3/39d1d366356dcf670b575137a66e2f40.jpg"
+            }
+            ]
+        },
+         {
+            "title": "Pets",
+            "isOwner": true,
+            "roomId": "123",
+            "ownerImage": "https://i.pinimg.com/564x/1e/39/86/1e39866c41e9e77d962a07b4577423c7.jpg",
+            "ownerName": "Me",
+            "boosted": false,
+            "isSpecialChatRoom": false,
+            "isLimitedRoom": false,
+            "userLimit": 0,
+            "isRoomFull": false,
+            "subs": [
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/24/ec/2a/24ec2af1b40d1979c2c368387d438c6b.jpg"
+            },
+            {
+                "subName": "Heda",
+                "subPhoto": "https://i.pinimg.com/564x/22/ba/9e/22ba9e9fc4c47e323c31a58fa746797a.jpg"
+            },
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/24/ec/2a/24ec2af1b40d1979c2c368387d438c6b.jpg"
+            },
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/22/ba/9e/22ba9e9fc4c47e323c31a58fa746797a.jpg"
+            }
+            ]
+        },
+        {
+            "title": "Pets",
+            "isOwner": true,
+            "roomId": "123",
+            "ownerImage": "https://i.pinimg.com/564x/bd/9e/9f/bd9e9f0fdd1d72a88a11c70460b53f43.jpg",
+            "ownerName": "WanHeda",
+            "boosted": false,
+            "isSpecialChatRoom": true,
+            "isLimitedRoom": false,
+            "userLimit": 0,
+            "isRoomFull": false,
+           "subs": [
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/24/ec/2a/24ec2af1b40d1979c2c368387d438c6b.jpg"
+            },
+            {
+                "subName": "Heda",
+                "subPhoto": "https://i.pinimg.com/564x/22/ba/9e/22ba9e9fc4c47e323c31a58fa746797a.jpg"
+            },
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/24/ec/2a/24ec2af1b40d1979c2c368387d438c6b.jpg"
+            },
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/22/ba/9e/22ba9e9fc4c47e323c31a58fa746797a.jpg"
+            }
+            ]
+        },
+        {
+            "title": "Pets",
+            "isOwner": true,
+            "roomId": "123",
+            "ownerImage": "https://i.pinimg.com/564x/a7/de/24/a7de24dc367e240a814a47fc378e8141.jpg",
+            "ownerName": "Me",
+            "boosted": false,
+            "isSpecialChatRoom": false,
+            "isLimitedRoom": false,
+            "userLimit": 0,
+            "isRoomFull": false,
+          "subs": [
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/24/ec/2a/24ec2af1b40d1979c2c368387d438c6b.jpg"
+            },
+            {
+                "subName": "Heda",
+                "subPhoto": "https://i.pinimg.com/564x/22/ba/9e/22ba9e9fc4c47e323c31a58fa746797a.jpg"
+            },
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/24/ec/2a/24ec2af1b40d1979c2c368387d438c6b.jpg"
+            },
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/22/ba/9e/22ba9e9fc4c47e323c31a58fa746797a.jpg"
+            }
+            ]
+        },
+        {
+            "title": "Pets",
+            "isOwner": true,
+            "roomId": "123",
+            "ownerImage": "https://i.pinimg.com/564x/a7/de/24/a7de24dc367e240a814a47fc378e8141.jpg",
+            "ownerName": "Me",
+            "boosted": false,
+            "isSpecialChatRoom": false,
+            "isLimitedRoom": false,
+            "userLimit": 0,
+            "isRoomFull": false,
+          "subs": [
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/24/ec/2a/24ec2af1b40d1979c2c368387d438c6b.jpg"
+            },
+            {
+                "subName": "Heda",
+                "subPhoto": "https://i.pinimg.com/564x/22/ba/9e/22ba9e9fc4c47e323c31a58fa746797a.jpg"
+            },
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/24/ec/2a/24ec2af1b40d1979c2c368387d438c6b.jpg"
+            },
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/22/ba/9e/22ba9e9fc4c47e323c31a58fa746797a.jpg"
+            }
+            ]
+        },
+        {
+            "title": "Chill",
+            "isOwner": false,
+            "roomId": "123",
+            "ownerImage": "https://i.pinimg.com/564x/bd/9e/9f/bd9e9f0fdd1d72a88a11c70460b53f43.jpg",
+            "ownerName": "Me",
+            "boosted": true,
+            "isSpecialChatRoom": true,
+            "isLimitedRoom": true,
+            "userLimit": 15,
+            "isRoomFull": true,
+            "subs": [
+            {
+                "subName": "Ted",
+                "subPhoto": "https://i.pinimg.com/564x/bd/9e/9f/bd9e9f0fdd1d72a88a11c70460b53f43.jpg"
+            },
+            {
+                "subName": "Heda",
+                "subPhoto": "https://i.pinimg.com/564x/39/d1/d3/39d1d366356dcf670b575137a66e2f40.jpg"
+            }
+            ]
+        }
+        ]
+    }"""
+
+}
